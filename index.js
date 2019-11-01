b14cef40db46cb3c6b338e6ae5b8a9ed6cbc0d2a
Noraimar Mora <noraimar.mora@gmail.com>
2019-10-31 20:36:49 -0400

    connect-timeout

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var cors = require('cors');
 var router = require('./router/router')
 var bodyParser = require('body-parser');
 var {PORT, SERVER_HOSTNAME} = require('./config');
+var timeout = require('connect-timeout')
 
 app.use(cors())
+app.use(timeout(1000))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -15,8 +17,14 @@ app.get('/', (req, res) => {
 
 app.use(router)
 
-var server = app.listen(PORT, () => {
+app.use(haltOnTimedout)
+
+function haltOnTimedout (req, res, next) {
+    if (req.timedout) res.status(504).send({status: 504})
+}
+
+app.listen(PORT, () => {
     console.log(`Server running correctly in the url: http://${SERVER_HOSTNAME}:${PORT}`);
 });
 
-server.setTimeout(1000);
+// server.setTimeout(10000);
